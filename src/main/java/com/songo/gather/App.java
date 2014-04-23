3799b858a40e713181b607426b5729d53eea2d0e
GsuNapoleon <gsu.chenbiao@gmail.com>
2014-04-23 16:05:40 +0800

    初始化项目结构

---
+++ src/main/java/com/songo/gather/App.java
@@ -0,0 +1,13 @@
+package com.songo.gather;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
